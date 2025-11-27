bcf7a11b2d8ded61288b2714b9d6d65fc834b121
Valentin Durand de Gevigney <valentin.durand.de.g@gmail.com>
2025-11-27 14:02:50 +0100

    Added mindful_subream to the list of modules archived by ExperimentRound

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from mindful_core.main import main
+from mindful_core.main import main as core_main
+from mindful_core.experiments.experiment_round import ExperimentRound
 
 # noinspection PyUnresolvedReferences
 from mindful_subream.models.classification.sieve import MultimodalSieve
+import mindful_subream
+
+
+def main():
+    ExperimentRound.archived_modules.append(mindful_subream)
+    core_main()
+
 
 if __name__ == "__main__":
     main()
